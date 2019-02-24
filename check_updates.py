1f720e2d094ccd2e487135e56b54894a81eeb0f2
realdip <realdip@gmail.com>
2019-02-24 20:52:18 +0200

    add

--- check_updates.py
+++ check_updates.py
@@ -1,4 +1,5 @@
 import requests
+from time import sleep
 
 url = "https://api.telegram.org/bot794555801:AAEos1HrFYUDst0orxtRZVjs_8QMIN-HsOM/"
 
@@ -12,3 +13,23 @@ def last_update(data):
     results = data['result']
     total_updates = len(results) - 1
     return results[total_updates]
+
+def get_chat_id(update):
+    chat_id = update['message']['chat']['id']
+    return chat_id
+
+def send_mess(chat, text):
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
+def main():
+    update_id = last_update(get_updates_json(url))['update_id']
+    while True:
+        if update_id == last_update(get_updates_json(url))['update_id']:
+           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
+           update_id += 1
+        sleep(3)
+
+if __name__ == '__main__':
+    main()
